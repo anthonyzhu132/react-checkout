929ca376c6a69f4b076c1b737505329b3c864386
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-04-15 00:24:38 -0700

    added api call to server to map through items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,12 @@ import Navigation from './components/navigation/navigation';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
+import { connect } from "react-redux";
+import { addProducts } from "./redux/cart/cartActions"
+import { useQuery } from 'react-query'
+import { getProducts } from './api';
+
+
 /*
  *** NOTES ***
 
@@ -25,36 +31,26 @@ import './App.css';
  TODO: AS A USER I CAN REMOVE ITEMS FROM THE CART
 */
 
-
-// REPLACE WITH API FETCHED ITEMS
-const TEMPORARY_ITEMS = [
-  {
-    id: 1,
-    name: 'Hat',
-    description:
-      'Fashion moves so quickly that, unless you have a strong point of view, you can lose integrity.',
-    imageSrc:
-      'https://media.istockphoto.com/photos/hat-on-white-background-picture-id526131500?b=1&k=20&m=526131500&s=170667a&w=0&h=TVhckgzmxLZ6b1V74eel7XbFy73tldESzBcH0ZG6g0c=',
-  },
-  {
-    id: 2,
-    name: 'Shirt',
-    description: 'Fashion never stops. There is always the new project, the new opportunity.',
-    imageSrc:
-      'https://media.istockphoto.com/photos/blank-white-tshirt-front-with-clipping-path-picture-id482948743?b=1&k=20&m=482948743&s=170667a&w=0&h=DetzN8rTsgQDTyBDSWvc7gUNz0gae0CUQecM-KNN3WY=',
-  },
-];
-
 const App = () => {
+  const { data } = useQuery(
+    ['products'], () => getProducts(), {
+      onSuccess: (data) => {
+        const productsArray = data?.products;
+        addProducts(productsArray)
+      }
+    }
+  )
+
   const { isOpen } = useContext(CartContext);
 
   return (
     <div className="App">
       <Navigation />
+
       {isOpen ? <Cart /> : null}
       <div className="products-listing">
-        {TEMPORARY_ITEMS.map((productItem) => (
-          <ProductCard key={productItem.id} product={productItem} />
+        {data?.products.map((product: any) => (
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
