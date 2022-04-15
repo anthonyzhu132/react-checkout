dc5d816e1dc2ac38b95fe329307044ca720612e9
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-04-15 12:00:52 -0700

    added redux connect to productcard

--- frontend/src/components/product-card/product-card.tsx
+++ frontend/src/components/product-card/product-card.tsx
@@ -1,6 +1,9 @@
 import { FC, ReactElement } from "react";
-
+import { connect } from "react-redux";
 import "./product-card.styles.css";
+import { addToCart } from "../../redux/cart/cartActions"
+
+import { Chip, Button, Dialog, MenuItem } from '@mui/material';
 
 export interface IProduct {
   name: string;
@@ -12,9 +15,10 @@ export interface IProduct {
 
 interface IProductCardProps {
   product: IProduct;
+  addToCart: Function;
 }
 
-const ProductCard: FC<IProductCardProps> = ({ product }): ReactElement => {
+const ProductCard: FC<IProductCardProps> = ({ product, addToCart }): ReactElement => {
   const { name, defaultImage, description } = product;
 
   return (
@@ -28,4 +32,10 @@ const ProductCard: FC<IProductCardProps> = ({ product }): ReactElement => {
   );
 };
 
-export default ProductCard;
+const mapDispatchToProps = (dispatch: (arg0: any) => any) => {
+  return {
+    addToCart: (id:string) => dispatch(addToCart(id))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ProductCard);
