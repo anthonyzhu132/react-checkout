5bb580f443fc25e6c4c0a38cf0e0358ec871123f
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-04-15 00:02:37 -0700

    added boilerplate for cart reducers

---
+++ frontend/src/redux/cart/cartReducer.js
@@ -0,0 +1,36 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CHANGE_QUANTITY,
+  ADD_PRODUCTS
+} from "./cartTypes";
+
+const INITIAL_STATE = {
+  products: [],
+  cart: [],
+}
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case ADD_PRODUCTS:
+      return {
+        ...state
+      }
+    case ADD_TO_CART:
+      return {
+        ...state
+      }
+    case REMOVE_FROM_CART:
+      return {
+        ...state
+      }
+    case CHANGE_QUANTITY:
+      return {
+        ...state
+      }
+    default:
+      return state;
+  }
+}
+
+export default cartReducer;
