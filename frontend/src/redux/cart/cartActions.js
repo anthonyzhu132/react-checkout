21735c8712bc83f45b66336020f1ea1700535686
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-04-14 23:50:43 -0700

    added boilerpalte for cart actions

---
+++ frontend/src/redux/cart/cartActions.js
@@ -0,0 +1,38 @@
+import { ADD_TO_CART, REMOVE_FROM_CART, CHANGE_QUANTITY, ADD_PRODUCTS } from "./cartTypes";
+
+export const removeFromCart = (id) => {
+  return {
+    type: REMOVE_FROM_CART,
+    payload: {
+      id
+    },
+  };  
+};
+
+export const addToCart = (id) => {
+  return {
+    type: ADD_TO_CART,
+    payload: {
+      id
+    },
+  };
+};
+
+export const changeQuantity = (id, value) => {
+  return {
+    type: CHANGE_QUANTITY,
+    payload: {
+      id,
+      quantity: value
+    }
+  }
+}
+
+export const addProducts = (products) => {
+  return {
+    type: ADD_PRODUCTS,
+    payload: {
+      products
+    },
+  };
+};
