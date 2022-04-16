03770ec8d7e647ac17d218c995dbd772d3d96a93
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-04-15 17:54:19 -0700

    removed mock data and rendered items that are in the cart state

--- frontend/src/components/cart/cart.tsx
+++ frontend/src/components/cart/cart.tsx
@@ -8,49 +8,36 @@ import { connect } from "react-redux";
 
 import './cart.styles.css';
 
-// REPLACE WITH YOUR OWN CART ITEMS & SOLUTION
-const TEMPORARY_ITEMS = [
-  {
-    id: 1,
-    name: 'Hat',
-    imageSrc:
-      'https://media.istockphoto.com/photos/hat-on-white-background-picture-id526131500?b=1&k=20&m=526131500&s=170667a&w=0&h=TVhckgzmxLZ6b1V74eel7XbFy73tldESzBcH0ZG6g0c=',
-    price: 15,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    name: 'Shirt',
-    imageSrc:
-      'https://media.istockphoto.com/photos/blank-white-tshirt-front-with-clipping-path-picture-id482948743?b=1&k=20&m=482948743&s=170667a&w=0&h=DetzN8rTsgQDTyBDSWvc7gUNz0gae0CUQecM-KNN3WY=',
-    price: 10,
-    quantity: 3,
-  },
-];
+interface ICartProps {
+  cart: [];
+}
 
-const Cart = () => {
+const Cart: FC<ICartProps> = ({ cart }) => {
   const { setIsOpen } = useContext(CartContext);
 
   const closeCart = () => setIsOpen(false);
-  const totalPrice = TEMPORARY_ITEMS.reduce((total, { price }) => total + price, 0).toFixed(2);
 
   return (
     <div className="cart-modal">
       <div className="cart-container">
-        <button className="close-button" onClick={closeCart}>
-          →
-        </button>
+        <ArrowForwardIcon className="close-button" onClick={closeCart} />
         <div className="cart-items-container">
-          {TEMPORARY_ITEMS.map((item) => (
-            <CartItem key={item.id} cartItem={item} />
+          {cart.map((item) => (
+            <CartItem cartItem={item} />
           ))}
         </div>
         <div className="total-container">
-          <span>Total: ${totalPrice}</span>
+          {/* <span>Total: ${totalPrice()}</span> */}
         </div>
       </div>
     </div>
   );
 };
 
-export default Cart;
+const mapStateToProps = (state: any) => {
+  return {
+    cart: state.cart.cart
+  }
+}
+
+export default connect(mapStateToProps)(Cart);
