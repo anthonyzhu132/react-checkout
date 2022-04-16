f69da5489fcd72eb0154a230d2fafcbbb561d56e
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-04-15 18:18:36 -0700

    added function to convert totals for each item and adjusted props to show correct info for each item in cart

--- frontend/src/components/cart-item/cart-item.tsx
+++ frontend/src/components/cart-item/cart-item.tsx
@@ -1,30 +1,64 @@
 import { FC, ReactElement } from 'react';
-
+import { connect } from "react-redux";
+import { removeFromCart } from '../../redux/cart/cartActions';
+import CloseIcon from '@mui/icons-material/Close';
 import './cart-item.styles.css';
+
+export interface IOptions {
+  type: string;
+  value: string;
+}
 
 export interface ICartItem {
   name: string;
-  imageSrc: string;
+  id: string;
+  defaultImage: string;
   quantity: number;
-  price: number;
+  priceCents: number;
+  description: string;
+  variantId: string;
+  type: IOptions;
+  variants: [{ priceCents: number }]
 }
 
 interface ICartItemProps {
   cartItem: ICartItem;
+  removeFromCart: Function;
 }
 
-const CartItem: FC<ICartItemProps> = ({ cartItem }): ReactElement => {
-  const { name, imageSrc, quantity, price } = cartItem;
+const CartItem: FC<ICartItemProps> = ({ cartItem, removeFromCart }): ReactElement => {
+  const { variantId, name, defaultImage, quantity, description, priceCents } = cartItem;
+
+  const toDollars = (num: number) => {
+    return num / 100;
+  }
+
+  const calculateItemTotal = (num: number, quantityInput: number) => {
+    return toDollars(num) * quantityInput;
+  }
+
   return (
     <div className="cart-item-container">
-      <img src={imageSrc} />
+      <img src={defaultImage} alt="Product"/>
       <div className="cart-item-details">
-        <span>{name} </span>
-        <span> Quantity: {quantity} </span>
-        <span> price: ${price.toFixed(2)} </span>
+        <CloseIcon className="remove-item-icon" onClick={() => removeFromCart(variantId)} />
+        <h4>{name}</h4>
+        <span className="cart-item-description">{description}</span>
+        <p> <b>Quantity: {quantity} </b></p>
+        <span>
+          <b>
+            price: ${calculateItemTotal(priceCents, quantity).toFixed(2)} 
+          </b>
+        </span>
       </div>
     </div>
   );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch: (arg0: any) => any) => {
+  return {
+    removeFromCart: (variantId:any) => dispatch(removeFromCart(variantId)),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CartItem);
